e4a8da7444a53c46679c238c2e1d904dece406d0
NiliXCIX <nilesh-bahoran@hotmail.com>
2021-11-16 16:08:12 +0100

    test

--- src/sample/JavaFXApp.java
+++ src/sample/JavaFXApp.java
@@ -20,7 +20,7 @@ public class JavaFXApp extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start (Stage primaryStage) throws Exception {
 
 
         Parent root;
