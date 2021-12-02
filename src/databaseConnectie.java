a09f7d7cdbdd88061ddbf218e11369b7c803698b
NiliXCIX <nilesh-bahoran@hotmail.com>
2021-12-02 15:26:03 +0100

    test database

--- src/databaseConnectie.java
+++ src/databaseConnectie.java
@@ -1,13 +1,26 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class databaseConnectie {
     public static void main (String [] args){
 
-        try {
-            Connection connectionString = DriverManager.getConnection("jdbc:mysql://MyDatabaseServer/MyDatabase", "root","1234");
-        } catch (Exception e) {
+        try{
+            Connection connectionString = DriverManager.getConnection("jdbc:mysql://localhost:3306/babbelbeestjedb", "root","1234");
+
+        Statement statement = connectionString.createStatement();
+
+        ResultSet resultset = statement.executeQuery("select * from gebruiker");
+
+        while (resultset.next()) {
+            System.out.println(resultset.getString("gebruikercode"));
+            System.out.println(resultset.getString("geslacht"));
+            System.out.println(resultset.getString("emailadres"));
+            System.out.println(resultset.getString("voornaam"));
+            System.out.println(resultset.getString("achternaam"));
+            System.out.println(resultset.getString("gebruikersnaam"));
+            System.out.println(resultset.getString("wachtwoord"));
+
+        }
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
